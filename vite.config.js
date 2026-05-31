80ba9dbfdd1359855fc7b7657ea18fecf64183a1
k-jojo3011 <kjoan3011@gmail.com>
2026-05-31 19:09:30 +0300

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Removed the base property so it defaults to "/" for Netlify
-});
+  base: '/Skyler-Fresh-Exporters/'
+})

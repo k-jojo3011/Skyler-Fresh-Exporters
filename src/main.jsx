2001cb91f832bb40849e05ed9a42550a101d5495
k-jojo3011 <kjoan3011@gmail.com>
2026-05-31 19:21:09 +0300

    Updated main and index.html files

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CurrencyProvider } from "./context/CurrencyContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Skyler-Fresh-Exporters">
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
+     <BrowserRouter basename="/Skyler-Fresh-Exporters">
+    <CurrencyProvider>
+      <App />
+    </CurrencyProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
+

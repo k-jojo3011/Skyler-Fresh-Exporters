ff21816bd06166bb6bb2b20a2cf4a7dd93f41de0
k-jojo3011 <kjoan3011@gmail.com>
2026-05-31 19:16:25 +0300

    Configured GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { CurrencyProvider } from "./context/CurrencyContext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CurrencyProvider>
-      <App />
-    </CurrencyProvider>
+    <BrowserRouter basename="/Skyler-Fresh-Exporters">
+      <CurrencyProvider>
+        <App />
+      </CurrencyProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
